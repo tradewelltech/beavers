9a9d56043510f829380f1388d5a39bc0e7e70283
aandres <aandres@trumidtech.com>
2026-03-30 14:55:11 +0100

    feat: always expect version

--- beavers/__init__.py
+++ beavers/__init__.py
@@ -1,8 +1,6 @@
 from beavers.dag import Dag, Node, TimerManager
 
-try:
-    from beavers._version import __version__
-except ModuleNotFoundError:
-    __version__ = "0.0.0"
+from beavers._version import __version__
+
 
 __all__ = ["Dag", "Node", "TimerManager"]
